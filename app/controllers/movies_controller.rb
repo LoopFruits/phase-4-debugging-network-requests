e9050f0f992fb030bdbb885917ceae1b8f3d53e6
LoopFruits <Kensworldc@gmail.com>
2022-04-08 13:27:51 -0400

    completed assingment, ran into issues because I kept closing the rails server

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,24 @@
 class MoviesController < ApplicationController
+
   
+  def create
+    # using byebug allows us to see all the key-value pairs such as title, year, length and so on
+    #byebug
+    movie = Movie.create(movie_params)
+    render json: movie, status: :created
+  end
+
+
   def index
     movies = Movie.all
     render json: movies
   end
+
+  private 
+
+  def movie_params 
+    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end
+
 
 end
